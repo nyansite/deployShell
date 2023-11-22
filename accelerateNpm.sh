94f6d329495ec5ce73f36fc3c0f4257cf7687ba6
flyingsnoopy <lijiaqi1835@outlook.com>
2023-11-22 15:38:10 +0800

    readme

--- accelerateNpm.sh
+++
@@ -1,3 +0,0 @@
-npm config set registry http://registry.npmmirror.com
-export npm_config_sharp_binary_host="https://npmmirror.com/mirrors/sharp"
-export npm_config_sharp_libvips_binary_host="https://npmmirror.com/mirrors/sharp-libvips"
