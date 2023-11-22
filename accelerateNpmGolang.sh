e56763cd744088440cfe19f1ad5d006d01877ef5
flyingsnoopy <lijiaqi1835@outlook.com>
2023-11-22 15:50:29 +0800

    deployDB debug

--- accelerateNpmGolang.sh
+++ accelerateNpmGolang.sh
@@ -1,5 +1,6 @@
 npm config set registry http://registry.npmmirror.com
 export npm_config_sharp_binary_host="https://npmmirror.com/mirrors/sharp"
 export npm_config_sharp_libvips_binary_host="https://npmmirror.com/mirrors/sharp-libvips"
+export PATH=$PATH:/usr/local/go/bin
 go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.cn
