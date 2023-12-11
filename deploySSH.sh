c22b0c2f64e71f71621056bbeaefb646bd9886b9
flyingsnoopy <lijiaqi1835@outlook.com>
2023-12-11 22:12:45 +0800

    now fetch stable verison of nodebb

--- deploySSH.sh
+++ deploySSH.sh
@@ -3,6 +3,6 @@ git clone git@github.com:nyansite/cute_site.git nyasiteBackend
 #deploy backend
 git clone git@github.com:nyansite/cirno_web.git nyasiteFrontend
 #deploy nodebb
-git clone git@github.com:NodeBB/NodeBB.git nyasiteNodeBB
+git clone -b v3.x git@github.com:NodeBB/NodeBB.git nyasiteNodeBB
 cd nyasiteNodeBB
 node nodebb setup
