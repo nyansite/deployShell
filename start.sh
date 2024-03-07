ec2048619eaebc4bb202dbd77ea4b686d9a7d8d4
flyingsnoopy <lijiaqi1835@outlook.com>
2024-03-07 18:19:41 +0800

    remove cgo

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ export PATH=$PATH:/usr/local/go/bin
 cd /nyasite/nyasiteNodeBB
 node nodebb start &
 cd /nyasite/nyasiteBackend/
-CGO_ENABLED=1 go run . &
+go run . &
 cd /nyasite/nyasiteFrontend
 npm run dev
